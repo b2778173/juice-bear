f55a07f5fe9162c5769df2b4ff495b894418f8ef
Cheng® <cheng106.andy@gmail.com>
2023-11-09 22:31:14 +0800

    Refactor: adjust paths, update configurations, and remove slave databases

---
+++ jb-framework/src/main/java/com/juice/framework/config/DruidConfig.java
@@ -0,0 +1,119 @@
+package com.juice.framework.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
+import com.alibaba.druid.util.Utils;
+import com.juice.framework.config.properties.DruidProperties;
+import com.juice.framework.datasource.DynamicDataSource;
+import com.juice.common.enums.DataSourceType;
+import com.juice.common.utils.JbSpringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.servlet.*;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author cheng
+ * @since 2023/11/4 3:22 PM
+ **/
+@Slf4j
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.master")
+    public DataSource masterDataSource(DruidProperties druidProperties) {
+        DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
+        return druidProperties.dataSource(dataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.slave")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.slave", name = "enabled", havingValue = "true")
+    public DataSource slaveDataSource(DruidProperties druidProperties) {
+        System.out.println("sssssss");
+        DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
+        return druidProperties.dataSource(dataSource);
+    }
+
+//    @Bean(name = "dynamicDataSource")
+//    @Primary
+//    public DynamicDataSource dataSource(DataSource masterDataSource) {
+//        Map<Object, Object> targetDataSources = new HashMap<>();
+//        targetDataSources.put(DataSourceType.MASTER.name(), masterDataSource);
+//        setDataSource(targetDataSources, DataSourceType.SLAVE.name(), "slaveDataSource");
+//        return new DynamicDataSource(masterDataSource, targetDataSources);
+//    }
+
+    /**
+     * 設定資料來源
+     *
+     * @param targetDataSources 備選資料來源集合
+     * @param sourceName        資料來源名稱
+     * @param beanName          bean名稱
+     */
+    public void setDataSource(Map<Object, Object> targetDataSources, String sourceName, String beanName) {
+        try {
+            DataSource dataSource = JbSpringUtils.getBean(beanName);
+            targetDataSources.put(sourceName, dataSource);
+        } catch (Exception e) {
+            log.error("ERR:{}", ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    /**
+     * 移除監控頁面底部的廣告
+     */
+    @Bean
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @ConditionalOnProperty(name = "spring.datasource.druid.statViewServlet.enabled", havingValue = "true")
+    public FilterRegistrationBean removeDruidFilterRegistrationBean(DruidStatProperties properties) {
+        // 取得web監控頁面的參數
+        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
+        // 提取出common.js的設定路徑
+        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
+        String commonJsPattern = pattern.replace("\\*", "js/common.js");
+        final String filePath = "support/http/resources/js/common.js";
+        // 建立filter進行過濾
+        Filter filter = new Filter() {
+            @Override
+            public void init(javax.servlet.FilterConfig filterConfig) {
+                log.info("===init===");
+            }
+
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                    throws IOException, ServletException {
+                chain.doFilter(request, response);
+                // 重置緩衝區，回應表頭不會被重置
+                response.resetBuffer();
+                // 取得common.js
+                String text = Utils.readFromResource(filePath);
+                // 用正則表達式替換banner, 移除底部的廣告訊息
+                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+                text = text.replaceAll("powered.*?shrek.wang</a>", "");
+                response.getWriter().write(text);
+            }
+
+            @Override
+            public void destroy() {
+                log.info("===destroy===");
+            }
+        };
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns(commonJsPattern);
+        return registrationBean;
+    }
+}
