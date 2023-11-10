82c82d226af7c3e44fe8b96a26edb77393031256
kevin tsai <b2778173@hotmail.com>
2023-11-10 12:40:07 +0800

    feat: update controllers.

--- jb-admin/src/main/java/com/juice/BearApplication.java
+++ jb-admin/src/main/java/com/juice/BearApplication.java
@@ -1,13 +1,38 @@
 package com.juice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
 public class BearApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BearApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+//		SpringApplication.run(BearApplication.class, args);
+		Environment env = SpringApplication.run(BearApplication.class, args).getEnvironment();
+
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+
+		log.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\t{}://localhost:{}\n\t" +
+						"External: \t{}://{}:{}\n\t" +
+						"Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				protocol,
+				env.getProperty("server.port"),
+				protocol,
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"),
+				env.getActiveProfiles());
 	}
 
 }
---
+++ jb-admin/src/main/java/com/juice/web/controller/monitor/SysLogininforController.java
@@ -0,0 +1,78 @@
+package com.juice.web.controller.monitor;
+
+import com.juice.common.annotation.Log;
+import com.juice.common.core.controller.BaseController;
+import com.juice.common.core.domain.AjaxResult;
+import com.juice.common.core.page.TableDataInfo;
+import com.juice.common.enums.BusinessType;
+import com.juice.common.utils.poi.ExcelUtil;
+import com.juice.framework.web.service.SysPasswordService;
+import com.juice.system.domain.SysLogininfor;
+import com.juice.system.service.ISysLogininforService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 系统访问记录
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/monitor/logininfor")
+public class SysLogininforController extends BaseController
+{
+    @Autowired
+    private ISysLogininforService logininforService;
+
+    @Autowired
+    private SysPasswordService passwordService;
+
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(SysLogininfor logininfor)
+    {
+        startPage();
+        List<SysLogininfor> list = logininforService.selectLogininforList(logininfor);
+        return getDataTable(list);
+    }
+
+    @Log(title = "登录日志", businessType = BusinessType.EXPORT)
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:export')")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysLogininfor logininfor)
+    {
+        List<SysLogininfor> list = logininforService.selectLogininforList(logininfor);
+        ExcelUtil<SysLogininfor> util = new ExcelUtil<SysLogininfor>(SysLogininfor.class);
+        util.exportExcel(response, list, "登录日志");
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:remove')")
+    @Log(title = "登录日志", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{infoIds}")
+    public AjaxResult remove(@PathVariable Long[] infoIds)
+    {
+        return toAjax(logininforService.deleteLogininforByIds(infoIds));
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:remove')")
+    @Log(title = "登录日志", businessType = BusinessType.CLEAN)
+    @DeleteMapping("/clean")
+    public AjaxResult clean()
+    {
+        logininforService.cleanLogininfor();
+        return success();
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:unlock')")
+    @Log(title = "账户解锁", businessType = BusinessType.OTHER)
+    @GetMapping("/unlock/{userName}")
+    public AjaxResult unlock(@PathVariable("userName") String userName)
+    {
+        passwordService.clearLoginRecordCache(userName);
+        return success();
+    }
+}
